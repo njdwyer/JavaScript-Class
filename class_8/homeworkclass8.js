ae4deba505d998b9b4654817e7fcc2c68ae2b2f4
njdwyer <dwyer@civicplus.com>
2017-07-13 17:17:50 -0500

    Class 8 stuff

---
+++ class_8/homeworkclass8.js
@@ -0,0 +1,47 @@
+let myHtml = {
+	tagName: "html",
+	children: [{
+			tagName: "head",
+			children: [{
+					tagName: "meta",
+					attributes: [{
+							charset: "UTF-8"
+						}
+					]
+				}, {
+					tagName: "title",
+					textContent: "A sample page"
+				}
+			]
+		}, {
+			tagName: "body",
+			children: [{
+					tagName: "div",
+					attributes: [{
+							id: "main"
+						}
+					],
+					children: [{
+							tagName: "h1",
+							attributes: [{
+									Class: "headline"
+								}
+							],
+							textContent: "Lorem ipsum"
+						}, {
+							tagName: "p",
+							textContent: "stuffs"
+						}
+
+					]
+				}
+			]
+		}
+	]
+};
+
+
+        {
+ 			tagName: "p",
+			textContent: "stuffs"
+ 		}
